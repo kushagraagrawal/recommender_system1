9c283d13ede53219178aae564002d32966351eeb
Kushagra Agrawal <kushagra106@gmail.com>
2016-12-22 15:57:03 +0530

    user based done

--- first_attempt/recommender.py
+++ first_attempt/recommender.py
@@ -11,13 +11,13 @@ f = open("ratings.csv")
 ratings_data = csv.reader(f)
 list_data = list(ratings_data)
 
-f1 = open("test.csv")
+f1 = open("toBeRated.csv")
 test = csv.reader(f1)
 test_data = list(test)
-#print test_data
+
 userratings = {}
 users = sorted(list_data, key = operator.itemgetter(0))
-#print users
+
 count = 0
 u_prev = 0
 '''
@@ -74,13 +74,69 @@ def sim_cosine(ratings, user_1, user_2):
 
 	return sum_xy/sqrt(sum_xx*sum_yy)
 
+def sim_pearson(ratings, user_1, user_2):
+	similarity = {}
+	for item in ratings[user_1]:
+		if item in ratings[user_2]:
+			similarity[item] = 1
+
+	#calculate the number of similarities 
+	numSim = len(similarity)
+
+	# If there is no similarity between them, return 0
+	if numSim == 0:
+		return 0
+
+	# Add the ratings from users
+	userOneSimArray = ([ratings[user_1][s] for s in similarity])
+	userOneSimArray = map(int, userOneSimArray)
+
+	sum_1 = sum(userOneSimArray)
+
+	userTwoSimArray = ([ratings[user_2][s] for s in similarity])
+	userTwoSimArray = map(int, userTwoSimArray)
+
+	sum_2 = sum(userTwoSimArray)
+
+	# Sum up the squares 
+
+	sum_1_sq = sum([pow(int(ratings[user_1][item]),2) for item in similarity])
+	sum_2_sq = sum([pow(int(ratings[user_2][item]),2) for item in similarity])
+
+	# Sum of the products
+	productSum = sum([int(ratings[user_1][item]) * int(ratings[user_2][item]) for item in similarity])
+	num = productSum - (sum_1*sum_2/numSim)
+	den = math.sqrt((sum_1_sq - pow(sum_1,2)/numSim) * (sum_2_sq - pow(sum_2,2)/numSim))
+
+	if den == 0:
+		return 0
+	r = num / den
+	return r
+
+def jaccard(ratings, user_1, user_2):
+	# similarity = {}
+	# for item in ratings[user_1]:
+	# 	if item in ratings[user_2]:
+	# 		similarity[item] = 1
+
+	# numSim =len(similarity)
+
+	# if numSim == 0:
+	# 	return 0
+
+	userOneRatingsArray = ([ratings[user_1][item] for item in ratings[user_1]])
+	userOne = set(userOneRatingsArray)
+	userTwoRatingsArray = ([ratings[user_2][item] for item in ratings[user_1]])
+	userTwo = set(userTwoRatingsArray)
+
+	return (len(set(userOne.intersection(userTwo))) / float(len(userOne.union(userTwo))))
 
 
 
 
 def matches (ratings,person,similarity):
 	first_person = person
-	scores = [(sim_cosine(ratings,first_person, second_person),second_person) for second_person in ratings if first_person!=second_person]
+	scores = [(similarity(ratings,first_person, second_person),second_person) for second_person in ratings if first_person!=second_person]
 	scores.sort()
 	scores.reverse()
 	return scores
@@ -98,19 +154,19 @@ def similaritems(ratings,similarity):
 	return itemlist
 
 def userBasedRecommendations(ratings, wantedPredictions, similarity):
-	file = open('user.txt','w')
+	file = open('user.txt', 'w')
 	ranks = {}
 
 	for tuple in wantedPredictions:
 		user = tuple[0]
 		movieAsked = tuple[1]
-
+		print user, movieAsked
 		total = {}
 		similaritySums = {}
 
 		for second_person in ratings:
 			if second_person == user: continue
-			s = sim_cosine(ratings,user, second_person)
+			s = similarity(ratings,user, second_person)
 
 			if s <= 0: continue
 
@@ -122,5 +178,79 @@ def userBasedRecommendations(ratings, wantedPredictions, similarity):
 					similaritySums[item] += s
 					ranks[item] = total[item]/similaritySums[item]
 		file.write(str(ranks[movieAsked])+'\n')
-userBasedRecommendations(userratings,test_data,'cosine')
+'''
+def itemBasedRecommendations(ratings, itemToMatch, wantedPredictions):
+	file = open('itemBasedRecos.txt', 'a')
+	for tuple in wantedPredictions:
+		user = tuple[0]
+		movieAsked = tuple[1]
+
+		uRatings = ratings[user]
+		scores = {}
+		total = {}
+		ranks = {}
+
+
+		# items rated by this user
+		for(item, rating) in uRatings.items():
+		# items that are similar to this one
+			for(similarity,item_2) in itemToMatch[item]:
+			# don't consider if the user has already rated this item
+				if item_2 in uRatings: continue
+				scores.setdefault(item_2, 0)
+				scores[item_2] += similarity*int(rating)
+
+				# sum over similarities
+				total.setdefault(item_2,0)
+				total[item_2] += similarity
+				if total[item_2] == 0: 
+					ranks[item_2] = 1
+				else:
+					ranks[item_2] = scores[item_2]/total[item_2]
+		print ranks[movieAsked]
+		file.write(str(ranks[movieAsked]))
+
+# combination of item based and used based recommendations. Content - Boosted Collaborative Filtering
+def itemBasedRecommendationsForCBCF(ratings, itemToMatch):
+	for user in ratings:
+		uRatings = ratings[user]
+		scores = {}
+		total = {}
+		ranks = {}
+
+
+		# items rated by this user
+		for(item, rating) in uRatings.items():
+		# items that are similar to this one
+			for(similarity,item_2) in itemToMatch[item]:
+			# don't consider if the user has already rated this item
+				if item_2 in uRatings: 
+					uRatings[item_2] = uRatings[item_2]
+				else:
+					scores.setdefault(item_2, 0)
+					scores[item_2] += similarity*int(rating)
+
+					# sum over similarities
+					total.setdefault(item_2,0)
+					total[item_2] += similarity
+					if total[item_2] == 0: 
+						uRatings[item_2] = 1
+					else:
+						uRatings[item_2] = scores[item_2]/total[item_2]
+		
+	return ratings
+'''
+def mainFunction():
+	similaritymeasure = raw_input()
+	if similaritymeasure == 'cosine':
+		sim = sim_cosine
+	elif similaritymeasure == 'pearson':
+		sim = sim_pearson
+	else:
+		sim = jaccard
+	print sim
+	userBasedRecommendations(userratings,test_data,sim)
+	
+
+mainFunction()
 
